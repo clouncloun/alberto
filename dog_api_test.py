4ec82622bc12956360e63f3ed16ff5c50dcf5454
Julia Davis <juliadav@umich.edu>
2025-04-15 18:23:39 -0400

    added breed_group variable to petfinder_pets.db

--- dog_api_test.py
+++ dog_api_test.py
@@ -42,12 +42,16 @@ conn = sqlite3.connect(path + "/" + "petfinder_pets.db")
 cur = conn.cursor()
 
 
-#cur.execute("""DROP TABLE doginfo""")
 
 # id tables setup
 cur.execute("""CREATE TABLE IF NOT EXISTS dog_bred_for (
     id INTEGER PRIMARY KEY, 
     dog_bred_for TEXT UNIQUE
+)""")
+
+cur.execute("""CREATE TABLE IF NOT EXISTS dog_breed_group (
+    id INTEGER PRIMARY KEY, 
+    dog_breed_group TEXT UNIQUE
 )""")
 
 cur.execute("""CREATE TABLE IF NOT EXISTS dog_lifespans (
@@ -72,15 +76,18 @@ CREATE TABLE IF NOT EXISTS doginfo (
     dog_temperament1_id INTEGER DEFAULT 0,
     dog_temperament2_id INTEGER DEFAULT 0,
     dog_bred_for_id INTEGER DEFAULT 0,
+    dog_breed_group_id INTEGER DEFAULT 0,
     dog_lifespan_id INTEGER DEFAULT 0,
     FOREIGN KEY (dog_temperament1_id) REFERENCES dog_temperament1(id),
     FOREIGN KEY (dog_temperament2_id) REFERENCES dog_temperament2(id),
     FOREIGN KEY (dog_bred_for_id) REFERENCES dog_bred_for(id),
+    FOREIGN KEY (dog_breed_group_id) REFERENCES dog_breed_group(id)
     FOREIGN KEY (dog_lifespan_id) REFERENCES dog_lifespans(id)
 )
 """)
 # for dog in doglist:
 #     print(dog)
+
 
 # putting data in the id tables
 # and ensure None values are replaced with 0 before inserting into the database
@@ -91,6 +98,8 @@ def insert_and_get_id(table, column, value):
     cur.execute(f"SELECT id FROM {table} WHERE {column} = ?", (value,))
     result = cur.fetchone()
     return result[0] if result else 0
+
+
 
 for dog in doglist:
     dog_breedname = dog['name']
@@ -114,18 +123,25 @@ for dog in doglist:
 
     dog_lifespan = dog['life_span']
     dog_bred_for = dog.get('bred_for', None)
+    dog_breed_group = dog.get('breed_group', None)
 
     dog_temperament1_id = insert_and_get_id('dog_temperament1', 'dog_temperament1', dog_temperament1)
     dog_temperament2_id = insert_and_get_id('dog_temperament2', 'dog_temperament2', dog_temperament2)
     dog_lifespan_id = insert_and_get_id('dog_lifespans', 'dog_lifespan', dog_lifespan)
     dog_bred_for_id = insert_and_get_id('dog_bred_for', 'dog_bred_for', dog_bred_for)
+    dog_breed_group_id = insert_and_get_id('dog_breed_group', 'dog_breed_group', dog_breed_group)
+
+    
 
     cur.execute("""
         INSERT OR IGNORE INTO doginfo 
-        (dog_breedname, dog_temperament1_id, dog_temperament2_id, dog_lifespan_id, dog_bred_for_id)
-        VALUES (?, ?, ?, ?, ?)""",
-        (dog_breedname, dog_temperament1_id, dog_temperament2_id, dog_lifespan_id, dog_bred_for_id)
+        (dog_breedname, dog_temperament1_id, dog_temperament2_id, dog_lifespan_id, dog_bred_for_id, dog_breed_group_id)
+        VALUES (?, ?, ?, ?, ?, ?)""",
+        (dog_breedname, dog_temperament1_id, dog_temperament2_id, dog_lifespan_id, dog_bred_for_id, dog_breed_group_id)
     )
+
+
+    # if you want new variable in doginfo.db just create new table from scratch instead
 
 
 conn.commit()
