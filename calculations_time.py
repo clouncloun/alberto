f6428288087e9baa4c5bec8affbac9923f194044
Audie <123757249+clouncloun@users.noreply.github.com>
2025-04-15 15:47:13 -0400

    hiii organized the data here to prepare for the calculations

--- calculations_time.py
+++ calculations_time.py
@@ -9,39 +9,65 @@ def get_data_from_table_as_dict(table_name):
     return [dict(row) for row in rows]
 
 
+# separate petfinder pets into cats and dogs
 catlist = []
+doglist = []
 pfinderdata = get_data_from_table_as_dict("petfinder")
 specieslist = get_data_from_table_as_dict("species")
+
+speciesdict = {}
+for item in specieslist:
+    speciesdict[item["id"]] = item ["specie"]
+
 for pet in pfinderdata:
-    petspeciesid = (pet["species"])
-    petspeciesname = 
+    petspeciesid = pet["species"]
+    petspeciesname = speciesdict.get(petspeciesid, "Unknown")
+    print(petspeciesname)
+    if petspeciesname.lower() == "cat":
+        catlist.append(pet)
+    if petspeciesname.lower() == "dog":
+        doglist.append(pet)
 
 
+# get breed from the id table for each cat in the catlist, each dog in the doglist
+breedslist = get_data_from_table_as_dict("breeds")
 
-# for creature in pfinderdata:
-#     species = (creature["species"])
-#     print(species)
+breeddict = {b["id"]: b["breed"] for b in breedslist}
+for cat in catlist:
+    breed_id = cat.get("breed")
+    breed_name = breeddict.get(breed_id, "Unknown")
+    cat["breed_name"] = breed_name
+for dog in doglist:
+    breed_id = dog.get("breed")
+    breed_name = breeddict.get(breed_id, "Unknown")
+    dog["breed_name"] = breed_name
 
-
-catswithbreeddata = []
-dogswithbreeddata = []
-
-catbreeddata = get_data_from_table_as_dict("catinfo")
-for cat in catbreeddata:
-    catbreed = (cat["cat_breedname"])
-    allbreeddata = get_data_from_table_as_dict("breeds")
-    for creature in allbreeddata:
-        creaturebreed = (creature['breed'])
-        if catbreed == creaturebreed:
-            catswithbreeddata.append(creature)
-
+    
+# makes a list of all the dogs that we have the breeds for in doginfo
+dogs_with_breed_data = []
 dogbreeddata = get_data_from_table_as_dict("doginfo")
-for dog in dogbreeddata:
-    dogbreed = (dog["dog_breedname"])
-    allbreeddata = get_data_from_table_as_dict("breeds")
-    for creature in allbreeddata:
-        creaturebreed = (creature['breed'])
-        if dogbreed == creaturebreed:
-            dogswithbreeddata.append(creature)
+for dog in doglist:
+    dogbreed = dog["breed_name"]
+    for breed in dogbreeddata:
+        breedname = breed["dog_breedname"]
+        if breedname == dogbreed:
+            dogs_with_breed_data.append(dog)
+            break
 
+print(dogs_with_breed_data)
+
+# finding what the most common breed is
+breed_counts = {}
+for dog in dogs_with_breed_data:
+    breed = dog["breed_name"]
+    if breed in breed_counts:
+        breed_counts[breed] += 1
+    else:
+        breed_counts[breed] = 1
+most_common_breed = None
+highest_breed_count = 0
+for breed, count in breed_counts.items():
+    if count > highest_breed_count:
+        most_common_breed = breed
+        highest_breed_count = count
 
